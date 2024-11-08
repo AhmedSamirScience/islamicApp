5ea038f54866decdc23bf521f3db4155ae40969f
ahmedsamirelbakrey <ahmed.samirelbakrey@gmail.com>
2024-11-09 01:35:34 +0200

    adding

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const express = require('express');
+const { default: helmet } = require('helmet');
 const app = express();
+
+
+// Middleware to parse the body of the request
+app.use (express.json());
+// Middleware to parse the body of the request
+app.use (express.urlencoded({ extended: true }));
+
+// Middleware to secure the app by setting various HTTP headers
+app.use(helmet());
+
+
+//port number to listen on 
+const port = process.env.PORT || 3000;
+
 
 
 app.get("/", (req, res) => {
@@ -8,6 +23,8 @@ app.get("/", (req, res) => {
  });
 
 
-app.listen(3000, () => {
-    console.log('Server is rddddunning on port 3000');
+
+ // listening 
+app.listen(port, () => {
+    console.log(`Server is rddddunning on port ${port}`);
 });
