0d40a74a62e305a128d10c4013cf97df4a30981b
ahmedsamirelbakrey <ahmed.samirelbakrey@gmail.com>
2024-11-08 21:50:18 +0200

    adding express

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-console.log("ahmed app running ")
+const express = require('express');
+const app = express();
 
- 
+
+app.get("/", (req, res) => {
+     res.send('Hello this is ');
+     console.log('ahmed sassssssssssssmir looggg this is ');
+ });
+
+
+app.listen(3000, () => {
+    console.log('Server is rddddunning on port 3000');
+});
